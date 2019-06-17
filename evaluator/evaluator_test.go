3b6f6fe8a1eee89208f6c2b8a8169039a4baacf2
Hani Benhabiles <kroosec@gmail.com>
2019-06-17 09:56:48 +0100

    3.10 Evaluate functions & function calls.
    Also add object/

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -177,6 +177,58 @@ func TestLetStatements(t *testing.T) {
 	}
 }
 
+func TestFunctionObject(t *testing.T) {
+	input := "fn(x) {x + 2;};"
+
+	evaluated := testEval(input)
+	fn, ok := evaluated.(*object.Function)
+	if !ok {
+		t.Fatalf("object is not a Function. got=%T", evaluated)
+	}
+
+	if len(fn.Parameters) != 1 {
+		t.Fatalf("function has wrong parameters. got=%+v", fn.Parameters)
+	}
+
+	if fn.Parameters[0].String() != "x" {
+		t.Fatalf("parameter is not 'x'. got=%q", fn.Parameters[0].String())
+	}
+	expected := "(x + 2)"
+	if fn.Body.String() != expected {
+		t.Fatalf("Erroneous body. expected=%q, got=%q", expected, fn.Body.String())
+	}
+}
+
+func TestFunctionApplication(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let identity = fn(x) {x; }; identity(5);", 5},
+		{"let identity = fn(x) { return x; }; identity(5);", 5},
+		{"let double = fn(x) { return x * 2; }; double(5);", 10},
+		{"let add = fn(x, y) { return x + y; }; add(5, 3);", 8},
+		{"let add = fn(x, y) { return x + y; }; add(5, add(4, 3));", 12},
+		{"fn(x, y) { return x + y; }(4, 8)", 12},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func TestClosures(t *testing.T) {
+	input := `
+let newAdder = fn(x) {
+  return fn(y) {x + y};
+}
+let addTwo = newAdder(2);
+addTwo(-3);
+`
+	testIntegerObject(t, testEval(input), -1)
+}
+
 func testEval(input string) object.Object {
 	lex := lexer.New(input)
 	parse := parser.New(lex)
